8ebfcfc5279a32f629eb00223cca15ef3e8a578a
Gabriel Castillo <gabriel.castillo86@hotmail.com>
2015-03-27 16:48:13 -0500

    Se agregan clases para uso de WebApi con un controlador generico

---
+++ src/Presentation/Nop.Web/Controllers/Api/CategoryController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Nop.Web.Controllers
+{
+    public class CategoryController : ApiController
+    {
+        // GET api/category
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/category/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/category
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/category/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/category/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ src/Presentation/Nop.Web/Infrastructure/RouteApiProvider.cs
@@ -0,0 +1,46 @@
+﻿using Autofac;
+using Autofac.Integration.WebApi;
+using Nop.Core.Infrastructure;
+using Nop.Web.Framework.Mvc.Routes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Http;
+
+namespace Nop.Web.Infrastructure
+{
+    public class RouteApiProvider : IRouteProvider
+    {
+        public void RegisterRoutes(System.Web.Routing.RouteCollection routes)
+        {
+            //var config = GlobalConfiguration.Configuration;
+            //WebApiConfig.Register(config);
+            var config = GlobalConfiguration.Configuration;
+
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            var builder = new ContainerBuilder();
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+            //builder.RegisterType<Nop.Web.Controllers.Api.CategoryController>().InstancePerRequest();
+            builder.Update(EngineContext.Current.ContainerManager.Container);
+            //var container = builder.Build();
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(EngineContext.Current.ContainerManager.Container);
+
+
+            GlobalConfiguration.Configuration.EnsureInitialized();
+        }
+
+        public int Priority
+        {
+            get { return 0; }
+        }
+    }
+}
